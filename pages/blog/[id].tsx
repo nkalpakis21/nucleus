18ccbb25511c179bf68e1854e70bd5857c6f0f38
Nicholas Kalpakis <nkalpakis@Nicholass-MacBook-Air.local>
2023-01-27 18:00:29 -0800

    Add comments UI.

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -2,6 +2,8 @@ import supabase from "../../utils/supabase";
 import YouTube from 'react-youtube';
 import MainAppBar from "../../components/app-bar/Main";
 import { Box, Container, Grid, Typography } from "@mui/material";
+import CommentComponent from "../../components/blog/comment";
+import { grey } from "@mui/material/colors";
 
 export async function getServerSideProps ({params}: any) {
     const {data: post, error} = await supabase.from('posts').select('*, comments(*)').eq('id', params.id).single();
@@ -16,15 +18,32 @@ export async function getServerSideProps ({params}: any) {
     redirect: 'follow'
     };
 
-    const result: any = await fetch(`https://api.twitter.com/2/tweets/search/recent?query=conversation_id:${post.conversation_id}&tweet.fields=in_reply_to_user_id,author_id`, requestOptions)
+    const result: any = await fetch(`https://api.twitter.com/2/tweets/search/recent?query=conversation_id:${post.conversation_id}&tweet.fields=in_reply_to_user_id,author_id,created_at&expansions=author_id`, requestOptions)
         .then(response => response.json())
         .then(result => result)
         .catch(error => console.log('error', error));
 
+
+    const comments = result.data;
+    const authors = result?.includes?.users;
+
+
+    console.log(result);
+    const tweets = comments?.map((mention: any) => {
+        const author = authors.find((author: any) => author.id === mention.author_id);
+        return {
+          tweet_id: mention.id,
+          text: mention.text,
+          username: `@${author.username}`,
+          name: author.name,
+          date: mention.created_at
+        };
+    });
+
     return {
         props: {
             post,
-            comments: result || {}
+            comments: tweets || []
 
         }
     }
@@ -44,11 +63,21 @@ export default function BlogPostPage({post, comments}: any) {
     return (
         <>
             <MainAppBar/>
-            {/* <pre>{JSON.stringify(comments, null, 2)}</pre> */}
             <Container maxWidth="md">
-                <Typography variant="h6">{post.title}</Typography>
-                <Typography variant="body1">{post.content}</Typography>
+                <Grid container sx={{mb: 2}}>
+                    <Grid item xs={6}>
+                        <Typography variant="h6">{post.title}</Typography>
+                        <Typography variant="body1">{post.content}</Typography>
+                    </Grid>
+                </Grid>
                 <YouTube videoId={post.youtube_id} opts={opts} onReady={onReady} />
+                <Grid container rowSpacing={3} sx={{mt: 3}}>
+                    {comments && (comments?.map((comment: any) => (
+                    <Grid item>
+                        <CommentComponent text={comment.text} author={comment.username} date={new Date(comment.date).toDateString()}/>
+                    </Grid>
+                    )))}
+                </Grid>
             </Container>
         </>
     )
