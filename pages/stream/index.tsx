dc6fc3a4dd80a0dffb10622fb3dbc6c0342d434f
Nicholas Kalpakis <nkalpakis21@gmail.com>
2023-02-02 09:23:48 -0800

    Add try catch around getting streams.

--- pages/stream/index.tsx
+++ pages/stream/index.tsx
@@ -10,10 +10,6 @@ import { TwitterApi } from 'twitter-api-v2';
 export async function getServerSideProps(context: any) {
     const {twitterToken, twitterSecret, twitterUserId} = getCookies({ req: context.req, res: context.res });
 
-    
-    console.log(process.env.NEXT_PUBLIC_TWITTER_CALLBACK_URL);
-    console.log(process.env.NEXT_PUBLIC_TWITTER_OAUTH_CLIENT_ID);
-    console.log(process.env.NEXT_PUBLIC_TWITTER_OAUTH_CLIENT_SECRET);
     if(!twitterToken) {
         const twitterClient = new TwitterApi({ clientId: process.env.NEXT_PUBLIC_TWITTER_OAUTH_CLIENT_ID!, clientSecret: process.env.NEXT_PUBLIC_TWITTER_OAUTH_CLIENT_SECRET! });
         const { url, codeVerifier, state } = await twitterClient.generateOAuth2AuthLink(process.env.NEXT_PUBLIC_TWITTER_CALLBACK_URL!, { scope: ['tweet.read', 'tweet.write', 'users.read', 'offline.access'] });
@@ -27,16 +23,33 @@ export async function getServerSideProps(context: any) {
             }
         }
     }
-    const twitterClient = new TwitterApi(twitterToken);
-    const user = await twitterClient.currentUserV2();
-    const userTimeline = await twitterClient.v2.userTimeline("148873990");
 
-    return {
-        props: {
-            posts: userTimeline.data.data,
-            user: user.data,
+    try{ 
+        const twitterClient = new TwitterApi(twitterToken);
+        const user = await twitterClient.currentUserV2();
+        const userTimeline = await twitterClient.v2.userTimeline("148873990");
+
+        return {
+            props: {
+                posts: userTimeline.data.data,
+                user: user.data,
+            }
+        };
+    } catch(e) {
+        const twitterClient = new TwitterApi({ clientId: process.env.NEXT_PUBLIC_TWITTER_OAUTH_CLIENT_ID!, clientSecret: process.env.NEXT_PUBLIC_TWITTER_OAUTH_CLIENT_SECRET! });
+        const { url, codeVerifier, state } = await twitterClient.generateOAuth2AuthLink(process.env.NEXT_PUBLIC_TWITTER_CALLBACK_URL!, { scope: ['tweet.read', 'tweet.write', 'users.read', 'offline.access'] });
+    
+        setCookie('twitterCodeVerifier', codeVerifier, { req: context.req, res: context.res, httpOnly: true });
+        setCookie('twitterState', state, { req: context.req, res: context.res, httpOnly: true });        
+
+        return {
+            props: {
+                twitterAuthLink: url,
+            }
         }
-    };
+    }
+
+    
 }
 
 export default function BlogListPage({posts, user, twitterAuthLink}: any) {
