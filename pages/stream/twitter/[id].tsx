bbcb49de093748e15911abf894c604e61a67b159
Nicholas Kalpakis <nkalpakis21@gmail.com>
2023-02-03 08:25:47 -0800

    Add redirect.

--- pages/stream/twitter/[id].tsx
+++ pages/stream/twitter/[id].tsx
@@ -1,19 +1,22 @@
-import supabase from "../../../utils/supabase";
-import YouTube from 'react-youtube';
 import MainAppBar from "../../../components/app-bar/Main";
-import { Box, Container, Grid, IconButton, Input, InputLabel, TextField, Typography } from "@mui/material";
+import { Box, Container, Grid, IconButton, Input, InputLabel, LinearProgress, Typography } from "@mui/material";
 import CommentComponent from "../../../components/blog/comment";
 import AddIcon from '@mui/icons-material/Add';
-import { getCookies, setCookie} from 'cookies-next';
+import { getCookies} from 'cookies-next';
 import { TwitterApi } from "twitter-api-v2";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 
 export async function getServerSideProps ({params, req, res}: any) {
     const {twitterToken} = getCookies({ req, res });
     
     if(!twitterToken) {
-        res.redirect(307, '/streams');
-        return;
+        return {
+            props: {
+                redirectUrl: '/stream'
+            }
+        }
     }
     
     try {
@@ -38,47 +41,51 @@ export async function getServerSideProps ({params, req, res}: any) {
         }
     } catch (e) {
         console.error(e);
-        res.redirect(307, '/streams');
-        return;
+        return {
+            props: {
+                redirectUrl: '/stream'
+            }
+        }
     }
 
 }
-export default function BlogPostPage({post, comments}: any) {
-    const opts = {
-        width: '50%',
-        playerVars: {
-          autoplay: 1,
-        },
-      };
-
-    const onReady = (event: any) => {
-        // event.target.pauseVideo();
-    }
+export default function BlogPostPage({post, comments, redirectUrl}: any) {
+    const router = useRouter();
 
     const addComment = async(e: any) => {
         e.preventDefault();
-
-
-        const response = await fetch('/api/twitter/post', {
+        await fetch('/api/twitter/post', {
             method: 'POST',
             body: JSON.stringify({"content": e.target.comment.value.toString(), "in_reply_to_status_id": post.data.id.toString()})
         })
-        console.log(e.target.comment.value.toString());
-        console.log(post.data.id.toString());
     }
 
+    useEffect(() => {
+        if(redirectUrl) {
+            router.push(redirectUrl);
+        }
+    }, [redirectUrl])
+
+    if(!redirectUrl) {
+        
+    }
 
     return (
         <>
             <MainAppBar/>
             <Container maxWidth="md">
                 <Grid container sx={{mb: 2}}>
-                    <Grid item xs={6}>
-                        <Typography variant="h6">{post.title}</Typography>
-                        <Typography variant="body1">{post.content}</Typography>
-                    </Grid>
+                    {post !== undefined ? (
+                        <Grid item xs={6}>
+                            <Typography variant="h6">{post.title}</Typography>
+                            <Typography variant="body1">{post.content}</Typography>
+                        </Grid>
+                    ):(
+                        <Box sx={{ width: '30%' }}>
+                            <LinearProgress />
+                        </Box>
+                    )}
                 </Grid>
-                <YouTube videoId={post.youtube_id} opts={opts} onReady={onReady} />
                 <Grid container sx={{mt: 4}}>
                     <form onSubmit={addComment}>
                         <Grid container alignItems="end">
@@ -89,9 +96,11 @@ export default function BlogPostPage({post, comments}: any) {
                             <IconButton 
                                 type="submit"
                                 aria-lable="add-comment" 
+                                disabled={!post}
                                 sx={{height: 20, width: 20, color: 'black', '&:hover': {
                                     backgroundColor: 'rgba(172, 225, 246, 0.16)',
                                     borderRadius: 1
+                                
                                 }}}>
                                 <AddIcon/>
                             </IconButton>
@@ -99,11 +108,11 @@ export default function BlogPostPage({post, comments}: any) {
                     </form>
                 </Grid>
                 <Grid container rowSpacing={3} mt={0.5}>
-                    {comments && (comments?.map((comment: any) => (
+                    {comments && comments.length > 0 ? (comments?.map((comment: any) => (
                     <Grid item key={comment.tweet_id}>
                         <CommentComponent text={comment.text} author={comment.username} date={new Date(comment.created_at).toDateString()}/>
                     </Grid>
-                    )))}
+                    ))) : (<Typography variant="h6" sx={{mt: 4}}>No comments yet!</Typography>)}
                 </Grid>
             </Container>
         </>
